72585ae24dc63cf5adeb1ddf174e568245a2a060
Florent Mouton <flormouton@gmail.com>
2021-12-24 09:47:22 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SocketService } from './socket';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, SocketService],
+})
+export class AppModule {}
